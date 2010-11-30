dcc5244e88729a1141350e686e749df303bac220
Michael Becker <michaelbecker@Michael-Beckers-MacBook.local>
2010-11-30 07:58:21 -0500

    improved trial look

--- app/scripts/trial.js
+++ app/scripts/trial.js
@@ -5,9 +5,11 @@ function Trial(screen) {
 
 	this.screen.base1 = "";
 	this.screen.base2 = "";
-	this.screen.deriv1 = "data/sounds/" + exp.getCurrentScreen()["file_l"];
-	this.screen.deriv2 = "";
-	this.screen.continuebutton = exp.getCurrentScreen().item + 'continue'
+	this.screen.deriv1 = "data/sounds/" + exp.getCurrentScreen().file_l;
+	this.screen.deriv2 = "data/sounds/" + exp.getCurrentScreen().file_pl_l;
+	this.screen.continuebutton = exp.getCurrentScreen().item + 'continue';
+	this.pl1button = exp.getCurrentScreen().item + 'pl1button'
+	this.pl2button = exp.getCurrentScreen().item + 'pl2button'
 
 	soundManager.createSound({
 		id: exp.getCurrentScreen().deriv1,
@@ -17,17 +19,41 @@ function Trial(screen) {
 		onload:function() {
 		},
 		onfinish:function() {
+			$("#" + exp.getCurrentScreen().item + 'secondhalf').show();
+		}
+	});
+
+	soundManager.createSound({
+		id: exp.getCurrentScreen().deriv2,
+		url: exp.getCurrentScreen().deriv2,
+		autoPlay: false, 
+		autoLoad: true,
+		onload:function() {
+		},
+		onfinish:function() {
 			$("#" + exp.getCurrentScreen().continuebutton).show();
 		}
 	});
 
+	
+	
+
 	this.html = function() {
+	
+		var f1 = this.screen.frame.replace(/_+/, "<B><I>" + this.screen.item + "</I></B>");
+		var b1 = '<INPUT TYPE="button" ID="' + exp.getCurrentScreen().pl1button +' " VALUE="    ►    "  onClick="soundManager.play(exp.getCurrentScreen().deriv1);">';
+		f1 = f1.replace(/_+/, b1);
+
+		var f2 = this.screen.frame.replace(/_+/, "<B><I>" + this.screen.item + "</I></B>");
+		var b2 = '<INPUT TYPE="button" ID="' + exp.getCurrentScreen().pl2button +' " VALUE="    ►    "  onClick="soundManager.play(exp.getCurrentScreen().deriv2);">';
+		f2 = f2.replace(/_+/, b2);
+		
 		var str = "";
 		str += "<P>" 
-		str += this.screen.item + "  --- " + this.screen.frame;
-		str += "<P>";
-		str += '<INPUT TYPE="button" VALUE="    ►    "  onClick="soundManager.play(exp.getCurrentScreen().deriv1);">';
-		str += "<P>";
+		str += f1;
+		str += '<DIV ID="' + exp.getCurrentScreen().item + 'secondhalf' + '" STYLE="display: none;">';
+		str += f2;
+		str += '</DIV>';
 		str += '<INPUT TYPE="button" ID="' + exp.getCurrentScreen().continuebutton  + '" VALUE="    continue    "  onClick="exp.advance(this);" STYLE="display: none;">';
 
 		return str;
