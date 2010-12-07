c0e640d0848e5b51648ee68ec83f1b5552586fa6
Jon Levine <tlozoot@gmail.com>
2010-12-06 20:39:12 -0500

    environments

--- app/config.js
+++ app/config.js
@@ -1,8 +1,13 @@
-function Config() {
-    this.proxyHost =  "http://phonetics.fas.harvard.edu/experigen_proxy/pass?url=";
-    this.baseURL = "http://static.jon-levine.com/experigen/";
+function Config(env) {
+    if (env == 'development') {
+        this.proxyHost =  "http://localhost/experigen_proxy/pass?url=";
+        this.baseURL = "http://localhost/experigen/";
+    } else if (env == 'production') {
+        this.proxyHost =  "http://phonetics.fas.harvard.edu/experigen_proxy/pass?url=";
+        this.baseURL = "http://static.jon-levine.com/experigen/";
+    }
     this.proxyURL = this.proxyHost + this.baseURL;
-    // this.proxyURL = "";
     this.experiment = "French";
+    this.title = "French Questionaire"
     this.databaseHost = "";
 }
