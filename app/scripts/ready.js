93af3a578184e3a148689c89a7e3ad7ea0a0fa74
Michael Becker <duck.prime@gmail.com>
2010-12-07 17:10:25 -0500

    more method chaining

--- app/scripts/ready.js
+++ app/scripts/ready.js
@@ -2,30 +2,31 @@ Experiment.prototype.initialize = function () {
 
 	var items = this.getItems();
 
-	var block0 = items.subset("item","farasel").pairWith("finalConsonant",["l"]);
-	block0.pairWith("frame", this.getFrameSentences().chooseFirst());
-	block0.pairWith("rand", ["x-first"]);
-	block0.shuffle();
+	var sampleItem = 
+	    items.subset("item","farasel").pairWith("finalConsonant",["l"])
+	         .pairWith("frame", this.getFrameSentences().chooseFirst())
+	         .pairWith("rand", ["x-first"])
+	         .shuffle();
 	
-	var block1 = new Array();
-	block1.append(items.exclude("item","farasel").subset("size","poly").subset("type","al-aux").chooseRandom(1))
-	      .append(items.subset("size","mono").subset("type","el-aux").chooseRandom(1))
-	      .pairWith("frame", this.getFrameSentences().excludeFirst().shuffle())
-	      .shuffle().pairWith("finalConsonant",["l","j"]).shuffle();
+	var nounBasedItems = 
+	    items.exclude("item","farasel").subset("size","poly").subset("type","al-aux").chooseRandom(1)
+	         .append(items.subset("size","mono").subset("type","el-aux").chooseRandom(1))
+	         .pairWith("frame", this.getFrameSentences().excludeFirst().shuffle())
+	         .shuffle().pairWith("finalConsonant",["l","j"]).shuffle();
 
-	var block2 = new Array();
-	block2.append(items.subset("size","poly").subset("type","ol-ou").chooseRandom(1))
-	      .append(items.subset("size","mono").subset("type","aul-au").chooseRandom(1))
-	      .pairWith("frame", this.getFrameSentences().excludeFirst().shuffle())
-	      .shuffle().pairWith("finalConsonant",["l"]);
+	var adjectiveBasedItems = 
+	    items.subset("size","poly").subset("type","ol-ou").chooseRandom(1)
+	         .append(items.subset("size","mono").subset("type","aul-au").chooseRandom(1))
+	         .pairWith("frame", this.getFrameSentences().excludeFirst().shuffle())
+	         .shuffle().pairWith("finalConsonant",["l"]);
 
-	block1.append(block2).shuffle();
-	block1.pairWith("rand", ["s-first","x-first"]).shuffle();
+	nounBasedItems.append(adjectiveBasedItems).shuffle()
+	              .pairWith("rand", ["s-first","x-first"]).shuffle();
 
 	this.addStaticScreen({url: "app/templates/intro.html"});
-	this.addBlock(block0);
+	this.addBlock(sampleItem);
 	this.addStaticScreen({url: "app/templates/getgoing.html"})
-	this.addBlock(block1);
+	this.addBlock(nounBasedItems);
 	this.addStaticScreen({url: "app/templates/demographic.html"})
 	this.addStaticScreen({url: "app/templates/finalthanks.html"})
 
