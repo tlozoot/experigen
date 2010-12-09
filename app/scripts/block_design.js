43e25e92089ee60897ddbff7fc34663d876aca7d
Michael Becker <michaelbecker@Michael-Beckers-MacBook.local>
2010-12-09 00:04:40 -0500

    shuffle no longer changes original array, pairWith accepts strings

--- app/scripts/block_design.js
+++ app/scripts/block_design.js
@@ -1,24 +1,25 @@
 Experiment.prototype.initialize = function () {
 
-	var items = this.getItems();
+	var items  = this.getItems();
+	var frames = this.getFrameSentences();
 
 	var sampleItem = 
-	    items.subset("item","farasel").pairWith("finalConsonant",["l"])
-	         .pairWith("frame", this.getFrameSentences().chooseFirst())
-	         .pairWith("rand", ["x-first"])
+	    items.subset("item","farasel").pairWith("finalConsonant","l")
+	         .pairWith("frame", frames.chooseFirst())
+	         .pairWith("rand", "x-first")
 	         .shuffle();
 	
 	var nounBasedItems = 
 	    items.exclude("item","farasel").subset("size","poly").subset("type","al-aux").chooseRandom(1)
 	         .append(items.subset("size","mono").subset("type","el-aux").chooseRandom(1))
-	         .pairWith("frame", this.getFrameSentences().excludeFirst().shuffle())
+	         .pairWith("frame", frames.excludeFirst().shuffle())
 	         .shuffle().pairWith("finalConsonant",["l","j"]).shuffle();
 
 	var adjectiveBasedItems = 
 	    items.subset("size","poly").subset("type","ol-ou").chooseRandom(1)
 	         .append(items.subset("size","mono").subset("type","aul-au").chooseRandom(1))
-	         .pairWith("frame", this.getFrameSentences().excludeFirst().shuffle())
-	         .shuffle().pairWith("finalConsonant",["l"]);
+	         .pairWith("frame", frames.excludeFirst().shuffle())
+	         .shuffle().pairWith("finalConsonant","l");
 
 	nounBasedItems.append(adjectiveBasedItems)
 	              .shuffle()
@@ -32,7 +33,8 @@ Experiment.prototype.initialize = function () {
 	    .addStaticScreen("demographic.html")
 	    .addStaticScreen("finalthanks.html");
 
-	this.setProgressBar(new Progressbar({adjustWidth: 4}))
-	    .advance();
+	this.setProgressBar(new Progressbar({adjustWidth: 4}));
+
+	this.advance();
 
 }
