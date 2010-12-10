eeb2eb813221e9e5d25a4d692acd63a2a0a4433a
Michael Becker <duck.prime@gmail.com>
2010-12-10 18:03:34 -0500

    deproxified

--- app/design.js
+++ app/design.js
@@ -1,41 +1,51 @@
 Experiment.prototype.initialize = function () {
-
+	
 	var items  = this.getItems();
 	var frames = this.getFrameSentences();
 	var pictures = this.getPictures();
 	
-	var sampleItem = 
-	    items.subset("item","farasel").pairWith("finalConsonant","l")
-	         .pairWith("frame", frames.chooseFirst())
-	         .shuffle()
-	         .pairWith("view","xfirst.ejs");
+	var sampleItem = items
+			.subset("item","farasel").pairWith("finalConsonant","l")
+			.pairWith("frame", frames.chooseFirst())
+			.shuffle()
+			.pairWith("view","xfirst.ejs")
+			;
 	
-	var nounBasedItems = 
-	    items.exclude("item","farasel").subset("size","poly").subset("type","al-aux").chooseRandom(1)
-	         .append(items.subset("size","mono").subset("type","el-aux").chooseRandom(1))
-	         .pairWith("frame", frames.excludeFirst().shuffle())
-	         .shuffle().pairWith("finalConsonant",["l","j"]).shuffle();
-
-	var adjectiveBasedItems = 
-	    items.subset("size","poly").subset("type","ol-ou").chooseRandom(1)
-	         .append(items.subset("size","mono").subset("type","aul-au").chooseRandom(1))
-	         .pairWith("frame", frames.excludeFirst().shuffle())
-	         .shuffle().pairWith("finalConsonant","l");
-
+	var nounBasedItems = items
+			.exclude("item","farasel")
+			.subset("size","poly")
+			.subset("type","al-aux")
+			.chooseRandom(1)
+			.append(items.subset("size","mono").subset("type","el-aux").chooseRandom(1))
+			.pairWith("frame", frames.excludeFirst().shuffle())
+			.shuffle()
+			.pairWith("finalConsonant",["l","j"])
+			.shuffle()
+			;
+	
+	var adjectiveBasedItems = items
+			.subset("size","poly").subset("type","ol-ou").chooseRandom(1)
+			.append(items.subset("size","mono").subset("type","aul-au").chooseRandom(1))
+			.pairWith("frame", frames.excludeFirst().shuffle())
+			.shuffle()
+			.pairWith("finalConsonant","l")
+			;
+	
 	nounBasedItems.append(adjectiveBasedItems)
-	              .shuffle()
-	              .pairWith("view",["sfirst.ejs","xfirst.ejs"])
-	              .shuffle();
-
-	this.addStaticScreen("intro.html"); // This file lives in app/templates/intro.html
-    this.addBlock(sampleItem);
+			.shuffle()
+			.pairWith("view",["sfirst.ejs","xfirst.ejs"])
+			.shuffle()
+			;
+	
+	
+	this.addStaticScreen("intro.html");
+	this.addBlock(sampleItem);
 	this.addStaticScreen("getgoing.html");
 	this.addBlock(nounBasedItems);
 	this.addStaticScreen("demographic.html");
 	this.addStaticScreen("finalthanks.html");
 
+	
 	this.setProgressBar(new Progressbar({adjustWidth: 4}));
-
 	this.advance();
-
 }
