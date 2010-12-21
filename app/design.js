c0855104f681c68b23090464b0ae9b318a70438c
kuzum99 <duck.prime@gmail.com>
2010-12-21 10:13:48 -0500

    text input 1

--- app/design.js
+++ app/design.js
@@ -5,47 +5,6 @@ Experigen.initialize = function () {
 	var items  = this.resource("items");
 	var frames = this.resource("frames");
 	var pictures = this.resource("pictures");
-	
-/*	var sampleItem = items
-			.subset("item","farasel").pairWith("finalConsonant","l")
-			.pairWith("frame", frames.chooseFirst())
-			.shuffle()
-			.pairWith("view","xfirst.ejs")
-			;
-	//console.log(sampleItem);
-
-	var nounBasedItems = items
-			.exclude("item","farasel")
-			.subset("size","poly")
-			.subset("type","al-aux")
-			.chooseRandom(1)
-			.concat(items.subset("size","mono").subset("type","el-aux").chooseRandom(1))
-			.pairWith("frame", frames.excludeFirst().shuffle())
-			.shuffle()
-			.pairWith("finalConsonant",["l","j"])
-			.shuffle()
-			;
-	//console.log(nounBasedItems);
-	
-	var adjectiveBasedItems = items
-			.subset("size","poly").subset("type","ol-ou").chooseRandom(1)
-			.concat(items.subset("size","mono").subset("type","aul-au").chooseRandom(1))
-			.pairWith("frame", frames.excludeFirst().shuffle())
-			.shuffle()
-			.pairWith("finalConsonant","l")
-			;
-	//console.log(adjectiveBasedItems);
-	
-	nounBasedItems = nounBasedItems
-	        .concat(adjectiveBasedItems)
-			.shuffle()
-			.pairWith("view",["sfirst.ejs","xfirst.ejs"])
-			.shuffle()
-			;
-	
-	//console.log(nounBasedItems);
-*/
-
 
 	var block1 =  items
 			.subset("type","al-aux").subset("size","poly").chooseRandom(2)
@@ -53,7 +12,7 @@ Experigen.initialize = function () {
 			.shuffle()
 			.pairWith("frame", frames.shuffle())
 			.shuffle()
-			.pairWith("view",["sfirst.ejs","xfirst.ejs"])
+			.pairWith("view",["sfirst.ejs"])
 			.shuffle()
 			;
 
@@ -67,7 +26,7 @@ Experigen.initialize = function () {
 			;
 
 
-	this.addStaticScreen("intro.ejs");
+	//this.addStaticScreen("intro.ejs");
 	this.addBlock(block1);
 	this.addStaticScreen("getgoing.ejs");
 	this.addBlock(block2);
