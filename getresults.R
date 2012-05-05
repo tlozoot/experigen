dc3f16014a9287441d5c9c164ba62187d9b634c7
Michael Becker <michael.becker@phonologist.org>
2012-05-05 19:39:07 -0400

    track your experiment's popularity

--- getresults.R
+++ getresults.R
@@ -4,6 +4,12 @@ experigen.sourceURL = "www.awesomeuniversity.edu.iamawesome.questionnaire"
 experigen.experimentName = "Default"
 experigen.database = "http://db.phonologist.org/"
 
+
+# check for usage of the experiment (number of page views per participant)
+experigen.users  =  paste(experigen.database, "users.cgi?experimentName=", experigen.experimentName, "&sourceurl=", experigen.sourceURL, sep="")
+read.csv(experigen.users, sep="\t")
+
+
 # read the experimental results from the server
 experigen.url  =  paste(experigen.database, "makecsv.cgi?experimentName=", experigen.experimentName, "&sourceurl=", experigen.sourceURL, sep="")
 exp  = read.csv(experigen.url, sep="\t")
@@ -13,5 +19,6 @@ meta$time = as.POSIXct(strptime(as.character(meta$time), "%a %b %d %H:%M:%S %Y")
 
 # assuming all went well, write to disk
 # so that the results are saved even after the database server is gone
+# it would be unwise not to keep a local copy of your results
 write.csv(exp, "exp.csv")
 write.csv(meta, "meta.csv")
